400ee7dfd9913eb542ab78bb3a6ff6da45513f53
Benoît Legat <benoit.legat@gmail.com>
2026-01-14 11:35:41 +0100

    Add tests

--- src/MacMPEC.jl
+++ src/MacMPEC.jl
@@ -3,23 +3,28 @@ module MacMPEC
 import DataFrames as DF
 import CSV
 
-const heading_meaning = CSV.read(joinpath(@__DIR__, "..", "data", "heading_meaning.csv"), DF.DataFrame)
-const collection = CSV.read(joinpath(@__DIR__, "..", "data", "collection.csv"), DF.DataFrame)
+const HEADING_MEANING = CSV.read(joinpath(@__DIR__, "..", "data", "heading_meaning.csv"), DF.DataFrame)
+const COLLECTION = CSV.read(joinpath(@__DIR__, "..", "data", "collection.csv"), DF.DataFrame)
 
-list() = collection.name
+heading_meaning() = HEADING_MEANING
+collection() = COLLECTION
+
+list() = collection().name
 
 @enum Status Optimal Infeasible Unknown
 
 struct Problem
     name::String
+    mod_file::String
+    dat_file::Union{String,Nothing}
     classification::String
     status::Status
     solution::Union{Float64,Nothing}
 end
 
 function problem(name::AbstractString)
-    idx = findfirst(==(name), collection.name)
-    sol = collection.solution[idx]
+    idx = findfirst(==(name), COLLECTION.name)
+    sol = COLLECTION.solution[idx]
     solution = nothing
     if sol == "(I)"
         status = Infeasible
@@ -29,7 +34,25 @@ function problem(name::AbstractString)
         status = Optimal
         solution = parse(Float64, sol)
     end
-    return Problem(name, collection.classification[idx], status, solution)
+    mod_file = COLLECTION[idx, "mod file"]
+    dat_file = COLLECTION[idx, "dat file"]
+    dat_file = dat_file == "n/a" ? nothing : dat_file
+    return Problem(name, mod_file, dat_file, COLLECTION.classification[idx], status, solution)
+end
+
+const PROBLEMS_DIR = joinpath(dirname(dirname(pathof(MacMPEC))), "data", "problems")
+
+function dat_path(p::Problem)
+    if isnothing(p.dat_file)
+        @assert !isfile(joinpath(PROBLEMS_DIR, "$(p.name).dat"))
+        return
+    end
+    # For `design-cent-21`, the `dat_file` is `design-cent-2.dat`, without the `1`
+    # so we cannot just do `"$(p.name).dat"`
+    path = joinpath(dirname(dirname(pathof(MacMPEC))), "data", "problems", p.dat_file)
+    @show p.dat_file
+    @assert isfile(path)
+    return path
 end
 
 end # module MacMPEC
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
 using Test
 import MacMPEC
 
-for name in MacMPEC.list()
-    @test MacMPEC.problem(name).name == name
+@testset "$name" for name in MacMPEC.list()
+    problem = MacMPEC.problem(name)
+    @test problem.name == name
+    collection = MacMPEC.collection()
+    idx = findfirst(==(name), collection.name)
+    sol = collection[idx, "dat file"]
+    dat_file = MacMPEC.dat_path(problem)
+    if sol == "n/a"
+        @test isnothing(dat_file)
+    else
+        @test dat_file isa String
+    end
 end
