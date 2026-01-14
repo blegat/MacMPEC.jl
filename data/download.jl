f4c80e02d668443374cd13cbbf8f7fa7012dde78
Benoît Legat <benoit.legat@gmail.com>
2026-01-14 11:51:59 +0100

    Add missing .dat

--- data/download.jl
+++ data/download.jl
@@ -6,6 +6,8 @@ using ProgressMeter
 import HTTP
 
 collection = CSV.read(joinpath(@__DIR__, "collection.csv"), DataFrame)
+
+# Update mod files
 @showprogress for row in eachrow(collection)
     mod_file = row["mod file"]
     if row.name == "bard1"
@@ -18,6 +20,20 @@ collection = CSV.read(joinpath(@__DIR__, "collection.csv"), DataFrame)
     response = HTTP.get(url)
     @assert response.status == 200
     open(joinpath(@__DIR__, "problems", mod_file), "w") do f
+        write(f, String(response.body))
+    end
+end
+
+# Update dat files
+@showprogress for row in eachrow(collection)
+    dat_file = row["dat file"]
+    if dat_file == "n/a"
+        continue
+    end
+    url = "http://www.mcs.anl.gov/~leyffer/MacMPEC/problems/$dat_file"
+    response = HTTP.get(url)
+    @assert response.status == 200
+    open(joinpath(@__DIR__, "problems", dat_file), "w") do f
         write(f, String(response.body))
     end
 end
