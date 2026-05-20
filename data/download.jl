1510213b4649617b30dabb128276d92579b15870
Benoît Legat <benoit.legat@gmail.com>
2026-05-20 08:28:59 +0200

    Fix

--- data/download.jl
+++ data/download.jl
@@ -12,8 +12,7 @@ collection = CSV.read(joinpath(@__DIR__, "collection.csv"), DataFrame)
     mod_file = row["mod file"]
     if row.name == "bard1"
         # It still fails because `HTTP.get` does not support HTTPS
-        url = "http://wiki.mcs.anl.gov/leyffer/images/1/1c/Bard1.mod"
-        mod_file = lowercase(mod_file)
+        url = "http://wiki.mcs.anl.gov/leyffer/images/1/1c/$mod_file"
     else
         url = "http://www.mcs.anl.gov/~leyffer/MacMPEC/problems/$mod_file"
     end
